7fc9d33c232c318862943ee50f36c79b324b6224
warabanshi <warabanshi@gmail.com>
2020-12-29 17:35:04 +0900

    Added install kubectl script

--- initialize/openSUSE-15.2.sh
+++ initialize/openSUSE-15.2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # see if this script has been run with sudo
-if [ `whoami` = 'root' ]; then
+if [ `whoami` != 'root' ]; then
     echo "run this script with sudo"
     exit 1
 fi
@@ -10,10 +10,19 @@ fi
 zypper in -y gcc zlib-devel readline-devel libbz2-devel libopenssl-devel sqlite3-devel docker
 
 # set launch daemon on boot
+echo "enable docker on boot"
 systemctl enable docker
 
 # install minikube
+echo "install minikube"
 curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
 install minikube-linux-amd64 /usr/local/bin/minikube
 rm minikube-linux-amd64
-usermod -aG docker $USER && newgrp docker
+usermod -aG docker $USER
+
+# install kubectl
+echo "install kubectl"
+curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl"
+chmod +x ./kubectl
+mv ./kubectl /usr/local/bin/kubectl
+
